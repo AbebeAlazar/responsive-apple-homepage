61d19ae7a68e3a997aec05e123bd7784052de9e1
Alazar Abebe <aliyabebe73@gmail.com>
2024-12-19 00:09:12 +0300

    responsiveness and footer toggle style.css script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// for footer exoanding
+document.querySelectorAll(".footer-links-wrapper h3").forEach(function(footer){
+	footer.addEventListener("click", function(){
+this.classList.toggle("expanded");
+    });
+    
+});
+
+
+  
